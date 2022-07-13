d5796b08129d3b1b23ccdf1b04e5d73cb28f5a01
unknown <jjatuah@gmail.com>
2022-07-13 16:00:45 +0100

    React App, file and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+ 
+
+const App = () => {
+  return ( 
+    <div className="App">
+      A fresh start
+    </div>
+   );
+}
+ 
+export default App;
