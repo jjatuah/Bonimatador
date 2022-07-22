ca79d5bdfaf751961e37a7d67b90c1d82c749ca5
unknown <jjatuah@gmail.com>
2022-07-22 15:37:57 +0100

    started the testimonials section and testimony component

---
+++ src/components/testimony/Testimony.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Testimony.css";
+
+
+
+const Testimony = () => {
+  return ( 
+    <div className="testimony">
+      
+    </div>
+   );
+}
+ 
+export default Testimony;
