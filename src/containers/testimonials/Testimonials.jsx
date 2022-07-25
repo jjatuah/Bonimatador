d53749d51222e110d50b458eeeea1dc87750b885
unknown <jjatuah@gmail.com>
2022-07-25 15:12:16 +0100

    changed the pattern for the testimony section to use a testimonyData array instead of a component accepting props

--- src/containers/testimonials/Testimonials.jsx
+++ src/containers/testimonials/Testimonials.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
 import "./Testimonials.css";
-import Testimony from '../../components/testimony/Testimony';
-import reviewImage1 from "../../assets/reviewImage1.png";
-import reviewImage2 from "../../assets/reviewImage2.png";
-import reviewImage3 from "../../assets/reviewImage3.png";
-import bgImage from "../../assets/testimonyBackground.jpg"
+import bgImage from "../../assets/testimonyBackground.jpg"; 
+import KeyboardArrowLeftRoundedIcon from '@material-ui/icons/KeyboardArrowLeftRounded';
+import KeyboardArrowRightRoundedIcon from '@material-ui/icons/KeyboardArrowRightRounded';
+import { testimonyData } from './testimonyData';
+  
 
 
 
 const Testimonials = () => {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = testimonyData.length;
+
+  const timeout = useRef(null)
+
+  useEffect(
+    () => {
+      const nextSlider = () => {
+        setCurrentSlide(currentSlide => (currentSlide === slideLength - 1 ? 0 : currentSlide + 1))
+      }
+
+      timeout.current = setTimeout(nextSlider, 10000);
+
+      return function() {
+        if (timeout.current) {
+          clearTimeout(timeout.current)
+        }  
+      }
+    }, [currentSlide, slideLength]
+  )
+
+  
+
+  const nextSlider = () => {
+
+    if (timeout.current) {
+      clearTimeout(timeout.current)
+    } 
+
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  }
+
+  const prevSlider = () => {
+
+    if (timeout.current) {
+      clearTimeout(timeout.current)
+    } 
+    
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1)
+  }
+
+  if(!Array.isArray(testimonyData) || testimonyData.length <= 0) {
+    return null
+  }
+
   return ( 
     <div className="testimonials">
       <div className="testimonialsbackground">
@@ -17,12 +63,29 @@ const Testimonials = () => {
       <div className="testimonyContainer">
 
         <h2>What are our customers saying ?</h2>
-
-        <Testimony img={reviewImage1} review={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit?"} name={"Rita pitsburg"} position={"CEO KRC ventures"}  />
-
-        {/* <Testimony img={reviewImage1} review={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit?"} name={"Rita pitsburg"} position={"CEO KRC ventures"}  /> 
-
-        <Testimony img={reviewImage1} review={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit?"} name={"Rita pitsburg"} position={"CEO KRC ventures"}  /> */}
+        {
+          testimonyData.map((slide, index) => {
+            return (
+              <>
+                {
+                  index === currentSlide && (
+                    <div className="testimony">
+                        <img src={slide.image} alt="" />
+                        <div className="testimonyContent">
+                          <p>{slide.testimony}</p>
+                          <h4>{slide.name}</h4>
+                          <h5 >{slide.position}</h5>
+                        </div>
+                        <KeyboardArrowLeftRoundedIcon style={{fontSize: "45px"}} className='left' onClick={prevSlider}/>
+                        <KeyboardArrowRightRoundedIcon style={{fontSize: "45px"}} 
+                        className='right' onClick={nextSlider}/>
+                    </div>
+                  )
+                }
+              </>
+            )
+          })
+        }
       </div>
     </div>
    );
---
+++ src/containers/testimonials/testimonyData.js
@@ -0,0 +1,25 @@
+import reviewImage1 from "../../assets/reviewImage1.png";
+import reviewImage2 from "../../assets/reviewImage2.png";
+import reviewImage3 from "../../assets/reviewImage3.png";
+
+
+export const testimonyData = [
+  {
+    image: reviewImage1,
+    testimony: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit",
+    name: " Jess Walsh",
+    position: "Manager KKG Group"
+  },
+  {
+    image: reviewImage2,
+    testimony: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit",
+    name: " Jess Walsh",
+    position: "Manager KKG Group"
+  },
+  {
+    image: reviewImage3,
+    testimony: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit",
+    name: " Jess Walsh",
+    position: "Manager KKG Group"
+  }
+]
