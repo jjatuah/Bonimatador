09711a1ee3af6c506f369854230b6e7af25c5b7a
unknown <jjatuah@gmail.com>
2022-07-25 22:52:27 +0100

    65% done with footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Footer.css";
-import { Facebook, Instagram, MailOutline, MessageRounded, Phone, Pinterest, Room, Twitter, WhatsApp, YouTube } from '@material-ui/icons';
+import { Facebook, Instagram, MailOutline, MessageRounded, Phone, Pinterest, Room, Twitter, WhatsApp,  ArrowRight } from '@material-ui/icons';
 
 
 
@@ -11,24 +11,24 @@ const Footer = () => {
         <div className="pageLinks">
           <h2>Links</h2>
           <div className="linksContainer">
-            <a href="#">Home</a>
-            <a href="#">About Us</a>
-            <a href="#">Our Value</a>
-            <a href="#">Services</a>
-            <a href="#">Properties</a>
-            <a href="#">Testimonials</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}Home</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}About Us</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}Our Value</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}Services</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}Properties</a>
+            <a href="#">{<ArrowRight style={{color: "#2196f3"}} />}Testimonials</a>
           </div>
         </div>
 
         <div className="contactLinks">
           <h2>Contacts</h2>
           <div className="contactsContainer">
-            <a href="#">{<Instagram />}Instagram</a>
-            <a href="#">{<Facebook/>}Facebook</a>
-            <a href="#">{<WhatsApp/>}WhatsApp</a>
-            <a href="#">{<Phone/>}Phone</a>
-            <a href="#">{<MailOutline/>}Email</a>
-            <a href="#">{<MessageRounded/>}Send a Message</a>
+            <a href="#">{<Instagram style={{color: "#2196f3"}}  />}Instagram</a>
+            <a href="#">{<Facebook style={{color: "#2196f3"}} />}Facebook</a>
+            <a href="#">{<WhatsApp style={{color: "#2196f3"}} />}WhatsApp</a>
+            <a href="#">{<Phone style={{color: "#2196f3"}} />}Phone</a>
+            <a href="#">{<MailOutline style={{color: "#2196f3"}} />}Email</a>
+            <a href="#">{<MessageRounded style={{color: "#2196f3"}} />}Send a Message</a>
           </div>
         </div>
       </div>
