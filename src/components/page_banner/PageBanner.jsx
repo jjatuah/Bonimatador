87987068b67f7829fe81f905d2ceb7fb9c82688b
unknown <jjatuah@gmail.com>
2022-07-27 07:31:08 +0100

    completed the page banner component

--- src/components/page_banner/PageBanner.jsx
+++ src/components/page_banner/PageBanner.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./PageBanner.css";
+import bg from "../../assets/testimonyBackground.jpg"
 
 
 
 const PageBanner = () => {
   return ( 
-    <div className="PageBanner">
-      
+    <div className="pageBanner">
+      <div className="pageBannerImage">
+        <img src={bg} alt="" />
+      </div>
+      <div className="pageBannerTitle">
+        <span>{"Rental Properties"}</span>
+        <h3>Have a wonderful experience at our shortlet</h3>
+      </div>
     </div>
    );
 }
