349fbc22d73d9d0cfabf938c8121b6f0c48c2f78
unknown <jjatuah@gmail.com>
2022-07-29 07:42:30 +0100

    completed the video component

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import "./Video.css";
 
 
-const Video = () => {
+const Video = ({poster, video ,type}) => {
   return ( 
     <div className="video">
-      
+      <video width="80%" height="80%" controls preload="none" poster={poster}>
+        <source src={video} type={type}/>
+        Your browser does not support the video tag.
+      </video>
     </div>
    );
 }
--- src/pages/rentals/Rentals.jsx
+++ src/pages/rentals/Rentals.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import "./Rentals.css";
 import PageBanner from '../../components/page_banner/PageBanner';
 import backgroundImg from "../../assets/testimonyBackground.jpg";
-
+import Video from '../../components/video/Video';
+import vid from "../../assets/Vid 3.mp4";
+import poster from "../../assets/About2.jpg";
 
 const Rentals = () => {
   return ( 
     <div className="rentals">
       <PageBanner img={backgroundImg} title={"Rental Properties"} cta={"Have a wonderful experience at our shortlet"} />
+      <Video poster={poster} video={vid} type={"video/mp4"} />
     </div>
    );
 }
