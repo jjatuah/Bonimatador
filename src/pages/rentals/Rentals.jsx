dd5fcfbcaf8b5c02736bd0b1b2f258c3d30234f6
unknown <jjatuah@gmail.com>
2022-07-29 09:00:21 +0100

    60% done with rentals page

--- src/pages/rentals/Rentals.jsx
+++ src/pages/rentals/Rentals.jsx
@@ -5,12 +5,30 @@ import backgroundImg from "../../assets/testimonyBackground.jpg";
 import Video from '../../components/video/Video';
 import vid from "../../assets/Vid 3.mp4";
 import poster from "../../assets/About2.jpg";
+import { CheckBoxOutlined } from '@material-ui/icons';
 
 const Rentals = () => {
   return ( 
     <div className="rentals">
       <PageBanner img={backgroundImg} title={"Rental Properties"} cta={"Have a wonderful experience at our shortlet"} />
-      <Video poster={poster} video={vid} type={"video/mp4"} />
+      
+      <div className="vidPitch">
+        <div className="vid">
+           <Video poster={poster} video={vid} type={"video/mp4"} />
+           <h4>Let's Walk you through our shortlet with this video</h4>
+        </div>
+
+        <div className="pitch">
+          <h3>Why our shortlets are the best ?</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quae inventore molestiae accusamus facere ducimus, magni quidem cumque debitis sed beatae totam, culpa dolore voluptatum incidunt hic quia, mollitia fugit?</p>
+          <ul>
+            <li>{<CheckBoxOutlined style={{color: "#2196f3", marginRight: "3.5px"}} />}How to support your Immune system</li>
+            <li>{<CheckBoxOutlined style={{color: "#2196f3", marginRight: "3.5px"}} />}A guide to 30 day fitness and workout challenges</li>
+            <li>{<CheckBoxOutlined style={{color: "#2196f3", marginRight: "3.5px"}} />}Guide to ease your back in the gym</li>
+            <li>{<CheckBoxOutlined style={{color: "#2196f3", marginRight: "3.5px"}} />}The mental health benefits of exercise</li>
+          </ul>
+        </div>
+      </div>
     </div>
    );
 }
