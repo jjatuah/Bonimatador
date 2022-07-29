f67b11d7b05b3eea1efb01c0e4b025e27573cc06
unknown <jjatuah@gmail.com>
2022-07-29 11:50:01 +0100

    started the PhotoBox component

---
+++ src/components/photobox/PhotoBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./PhotoBox.css";
+
+
+
+const PhotoBox = () => {
+  return ( 
+    <div className="photoBox">
+      
+    </div>
+   );
+}
+ 
+export default PhotoBox;
