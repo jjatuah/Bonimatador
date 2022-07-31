305b87476155c1069b72b6e81db759333c0852d5
unknown <jjatuah@gmail.com>
2022-07-31 11:20:46 +0100

    completed the homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import "./Homepage.css";
+import Banner from '../../containers/banner/Banner';
+import About from '../../containers/about/About';
+import Value from '../../containers/value/Value';
+import Services from '../../containers/services/Services';
+import FeaturedProperties from '../../containers/featured properties/FeaturedProperties';
+import Contact from '../../components/contact/Contact';
+import Testimonials from '../../containers/testimonials/Testimonials';
 
 
 const Homepage = () => {
   return ( 
     <div className="homepage">
-      
+      <Banner />
+      <About />
+      <Value />
+      <Services />
+      <FeaturedProperties />
+      <Testimonials />
+      <Contact />
     </div>
    );
 }
