6ae732c296d56f43b85b7ae18413d7e431c2de4e
unknown <jjatuah@gmail.com>
2022-08-06 21:38:01 +0100

    fixed the video size bug

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -5,7 +5,7 @@ import "./Video.css";
 const Video = ({poster, video ,type}) => {
   return ( 
     <div className="video">
-      <video width="80%" height="80%" controls preload="none" poster={poster}>
+      <video controls preload="none" poster={poster}>
         <source src={video} type={type}/>
         Your browser does not support the video tag.
       </video>
