ecedcec4247890ba88d9c4cb25ccbbca9f27d8e5
unknown <jjatuah@gmail.com>
2022-09-10 07:29:03 +0100

    updated navbar logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "./Navbar.css";
 import { Close, Menu } from '@material-ui/icons';
-import logo from "../../assets/improvedCrop2.jpg";
+import logo from "../../assets/BoniMatador1.png";
 
 
 const Navbar = () => {
